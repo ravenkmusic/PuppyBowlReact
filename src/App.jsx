d93653b70c1a5fa26848f3f76f85eb34f0e523f6
Raven Hendricks <ravenkbeats@gmail.com>
2024-03-10 13:44:05 -0400

    removed uncessary import statements

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import AllPlayers from "./src/components/AllPlayers";
-import SinglePlayer from "./src/components/SinglePlayer";
-import NavBar from "./src/components/NavBar";
+import AllPlayers from "./AllPlayers";
+import SinglePlayer from "./SinglePlayer";
 import './App.css'
 
 function App() {
