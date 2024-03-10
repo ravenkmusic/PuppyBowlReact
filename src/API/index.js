80c02e23c0b3982bee1d39d69248a37a0899795d
Raven Hendricks <ravenkbeats@gmail.com>
2024-03-10 15:47:23 -0400

    removed unnecessary code

--- src/API/index.js
+++ src/API/index.js
@@ -1,16 +1,8 @@
 const API_URL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohort}/players`;
 const cohort = `2308-acc-et-web-pt-b`;
 
-const state = {
-    players: [],
-};
-
 export async function fetchAllPlayers(){
-    try {
-        const response = await fetch(`${API_URL}`);
-        const result = await response.json();
-        state.players = result.data;
-    } catch (error) {
-        console.error(error);
-    }
+    const response = await fetch(`${API_URL}`);
+    const result = await response.json();
+    return result.data.players;
 }
--- src/components/AllPlayers.jsx
+++ src/components/AllPlayers.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { fetchAllPlayers } from ".api/index.js";
+
 export default function AllPlayers(){
     return (
         <h1>This is the AllPlayers component.</h1>
