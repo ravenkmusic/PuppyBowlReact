5ad6071eefa7e8a6b037b596a5532678af6aa9bb
Raven Hendricks <ravenkbeats@gmail.com>
2024-03-10 13:34:52 -0400

    fixed typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import AllPlayers from "./components/AllPlayers";
 import SinglePlayer from "./components/SinglePlayer";
+import NavBar from "./components/NavBar";
 import './App.css'
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path='/' element={<AllPlayers/>} />
+          <Route path='/' element={<AllPlayers />} />
           <Route path='/players/:id' element={<SinglePlayer />} />
       </Routes>
     </BrowserRouter>
