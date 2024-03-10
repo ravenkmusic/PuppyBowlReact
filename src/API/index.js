21ef9755c0c8257595145f27163762540ac526ac
Raven Hendricks <ravenkbeats@gmail.com>
2024-03-10 16:57:02 -0400

    added default keyword to function

--- src/API/index.js
+++ src/API/index.js
@@ -1,7 +1,7 @@
 const API_URL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohort}/players`;
 const cohort = `2308-acc-et-web-pt-b`;
 
-export async function fetchAllPlayers(){
+export default async function fetchAllPlayers(){
     const response = await fetch(`${API_URL}`);
     const result = await response.json();
     return result.data.players;
