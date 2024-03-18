ae2ac9ce8caa12268a22286b6d1731e13230d91b
Raven Hendricks <ravenkbeats@gmail.com>
2024-03-18 13:19:35 -0400

    changed styling and syntax to neatly generate players

--- src/API/index.js
+++ src/API/index.js
@@ -1,8 +1,8 @@
-const API_URL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohort}/players`;
 const cohort = `2308-acc-et-web-pt-b`;
+const API_URL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohort}/players`;
 
-export default async function fetchAllPlayers(){
-    const response = await fetch(`${API_URL}`);
-    const result = await response.json();
-    return result.data.players;
+export async function fetchAllPlayers(){
+        const response = await fetch(`${API_URL}`);
+        const result = await response.json();
+        return result.data.players;   
 }
--- src/components/AllPlayers.jsx
+++ src/components/AllPlayers.jsx
@@ -1,20 +1,34 @@
-/*import { useState, useEffect } from "react";
-import fetchAllPlayers from "../API/index.js";
-*/
+import { useState, useEffect } from 'react';
+import { fetchAllPlayers } from '../API/index';
 
 export default function AllPlayers(){
    
-    /* const [players, setPlayers] = useState([]);
+    const [players, setPlayers] = useState([]);
 
     useEffect(() =>{
         async function getAllPlayers(){
-            const players = await fetchAllPlayers();
-            setPlayers(players);
+                const players = await fetchAllPlayers();
+                setPlayers(players);
         }
         getAllPlayers();
-    }, []); */
+    }, []);
     
      return (
-        <h1>All Players!</h1>
-    )
+     <>
+        <div>
+            <h3>Players!</h3>
+        </div>
+        <div>
+            {
+            players.map((player)=>{
+                    return <div key={player.id}>
+                        <img src= {player.imageUrl} />
+                        <h4 id="puppy-name">{player.name}</h4>
+                        <p><span className="attribute">Breed:</span> {player.breed}</p>
+                        <p><span className="attribute">Current status:</span> {player.status}</p>
+                    </div>
+            })
+        }
+        </div>
+    </>)
 }
